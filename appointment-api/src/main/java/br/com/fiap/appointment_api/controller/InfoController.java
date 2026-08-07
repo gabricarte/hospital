b047633a8cb92068c8cfbe08acbec0bc3c26b166
leticiafdepaula <leticiaferreira150998@gmail.com>
2026-08-06 22:50:41 -0300

    chore(infra): atualizacao de dependencia e config de infra

--- appointment-api/src/main/java/br/com/fiap/appointment_api/controller/InfoController.java
+++ appointment-api/src/main/java/br/com/fiap/appointment_api/controller/InfoController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tools.jackson.databind.ObjectMapper;
-
 import java.time.LocalDateTime;
 
 @RestController
